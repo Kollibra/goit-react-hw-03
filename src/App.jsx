5bd31138d0bad246cb21ca3271e4cdba9b72f599
Iryna Kovalova <kovra777@gmail.com>
2024-02-22 00:04:08 +0200

    App

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import ContactList from './ContactList/ContactList';
+import SearchBox from './SearchBox/SearchBox';
+import ContactForm from './ContactForm/ContactForm';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+	const [contacts, setContacts] = useState(() => {
+		const savedContacts = window.localStorage.getItem('saved-contacts');
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+		if (savedContacts !== null) {
+			return JSON.parse(savedContacts);
+		}
+		return [
+			{ id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+			{ id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+			{ id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+			{ id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+		];
+	});
 
-export default App
+	const [filterValue, setFilter] = useState('');
+
+	useEffect(() => {
+		window.localStorage.setItem('saved-contacts', JSON.stringify(contacts));
+	}, [contacts]);
+
+	const filteredContacts = contacts.filter(contact =>
+		contact.name.toLowerCase().includes(filterValue.toLowerCase())
+	);
+
+	const addContact = newContact => {
+		setContacts(() => {
+			return [...contacts, newContact];
+		});
+	};
+
+	const removeContact = id => {
+		setContacts(() => {
+			return contacts.filter(contact => contact.id !== id);
+		});
+	};
+
+	return (
+		<>
+			<h1>Phonebook</h1>
+			<ContactForm contacts={contacts} onAddContact={addContact} />
+			<SearchBox value={filterValue} onChange={setFilter} />
+			<ContactList
+				contacts={filteredContacts}
+				onRemoveContact={removeContact}
+			/>
+		</>
+	);
+};
+
+export default App;
